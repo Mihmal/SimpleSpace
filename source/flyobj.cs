3de7931a1926cb96ecdb90a007ed0da0f3f8f1a0
Mihmal <mihmal98@gmail.com>
2014-06-03 09:45:55 +0300

    Заготовка класса под небесное тело

---
+++ source/flyobj.cs
@@ -0,0 +1,19 @@
+public class FlyObject 
+{
+
+  public string name
+  public double mass
+  public double x
+  public double y
+  public double vx
+  public double vy
+  
+  public FlyObject(string newName, double newMass, double newX, double newY, double newVx, double newVy) {
+    name = newName
+    mass = newMass
+    x = newX
+    y = newY
+    vx = newVx
+    vy = newVy
+  }
+}
