891bd746c5884c7433d4ac1edcfaaf90f30363f3
Mihmal <mihmal98@gmail.com>
2014-06-06 15:59:19 +0300

    Минорные фикмы
    Запустил в юнити, поправил мелкие косяки.

--- source/flyobj.cs
+++ source/flyobj.cs
@@ -1,67 +1,70 @@
-public class FlyObject 
-{
-  public double T = 0.01
-  public string name
-  public double mass//Две "s" по-приколу написал
-  public Vector2 velocity
-  public GameObject[] others
-  
-  double fx (localX) {
-    double a = 0;
-    for (int i=0; i<others.Length; i++) {
-      r = Vector2(others[i].transform.position.x-localX, 
-                  others[i].transform.position.y-transform.position.y)
-                  .magnitude;
-      a += others[i].mass*(others[i].transform.position.x - transform.position.x)/Math.pow(r, 3);
-    }
-    return a;
-  }
-  
-  double fy (localY) {
-    double a = 0;
-    for (int i=0; i<others.Length; i++) {
-      r = Vector2(others[i].transform.position.y-localY, 
-                  others[i].transform.position.x-transform.position.x)
-                  .magnitude;
-      a += others[i].mass*(others[i].transform.position.y - transform.position.y)/Math.pow(r, 3);
-    }
-    return a;
-  }
-  
-  void CalcX () {
-    double k1 = T*fx(transform.position.x);
-    double q1 = T*velocity.x;
-    
-    double k2 = T*fx(transform.position.x + q1/2);
-    double q2 = T*(velocity.x + k1/2);
-    
-    double k3 = T*fx(transform.position.x + q2/2);
-    double q3 = T*(velocity.x + k2/2);
-    
-    double k4 = T*fx(transform.position.x + q3);
-    double q4 = T*(velocity.x + k3);
-    
-    velocity.x += Time.deltaTime*(k1 + 2 * k2 + 2 * k3 + k4) / 6;
-  }
-  
-  void CalcY () {
-    double k1 = T*fy(transform.position.y);
-    double q1 = T*velocity.y;
-    
-    double k2 = T*fy(transform.position.y + q1/2);
-    double q2 = T*(velocity.y + k1/2);
-    
-    double k3 = T*fy(transform.position.y + q2/2);
-    double q3 = T*(velocity.y + k2/2);
-    
-    double k4 = T*fy(transform.position.y + q3);
-    double q4 = T*(velocity.y + k3);
-    
-    velocity.y += Time.deltaTime*(k1 + 2 * k2 + 2 * k3 + k4) / 6;
-  }
-  
-  void FixedUpdate () {
-    calcX();
-    calcY();
-    transform.translate(Time.deltaTime*velocity.x, Time.deltaTime*velocity.y, 0);
-  }
+using UnityEngine;
+using System.Collections;
+
+
+public class FLyObject : MonoBehaviour {
+
+	public float T = 0.01f;
+	public float mass;
+	public Vector2 velocity;
+	public GameObject[] others;
+	public FLyObject[] othersScripts;
+			
+	private float fx (float localX) {
+		float a = 0;
+		for (int i=0; i<others.Length; i++) {
+			float r = new Vector2(others[i].transform.position.x-localX, others[i].transform.position.y-transform.position.y).magnitude;
+			a += othersScripts[i].mass*(others[i].transform.position.x - transform.position.x)/Mathf.Pow(r, 3);
+		}
+		return a;
+	}
+	
+	float fy (float localY) {
+		float a = 0;
+		for (int i=0; i<others.Length; i++) { 
+			float r = new Vector2(others[i].transform.position.y-localY, others[i].transform.position.x-transform.position.x).magnitude;
+			a += othersScripts[i].mass*(others[i].transform.position.y - transform.position.y)/Mathf.Pow(r, 3);
+		}
+		return a;
+	}
+	
+	void CalcX () {
+		float k1 = T*fx(transform.position.x);
+		float q1 = T*velocity.x;
+
+		float k2 = T*fx(transform.position.x + q1/2);
+		float q2 = T*(velocity.x + k1/2);
+		
+		float k3 = T*fx(transform.position.x + q2/2);
+		float q3 = T*(velocity.x + k2/2);
+		
+		float k4 = T*fx(transform.position.x + q3);
+		float q4 = T*(velocity.x + k3);
+		
+		velocity.x += Time.deltaTime*(k1 + 2 * k2 + 2 * k3 + k4) / 6;
+		transform.Translate ((Time.deltaTime * (q1 + 2 * q2 + 2 * q3 + q4) / 6), 0f, 0f);
+	}
+	
+	void CalcY () {
+		float k1 = T*fy(transform.position.y);
+		float q1 = T*velocity.y;
+		
+		float k2 = T*fy(transform.position.y + q1/2);
+		float q2 = T*(velocity.y + k1/2);
+		
+		float k3 = T*fy(transform.position.y + q2/2);
+		float q3 = T*(velocity.y + k2/2);
+		
+		float k4 = T*fy(transform.position.y + q3);
+		float q4 = T*(velocity.y + k3);
+		
+		velocity.y += Time.deltaTime*(k1 + 2 * k2 + 2 * k3 + k4) / 6;
+		transform.Translate (0f, (Time.deltaTime * (q1 + 2 * q2 + 2 * q3 + q4) / 6),  0f);
+	}
+	
+	void FixedUpdate () {
+		CalcX();
+		CalcY();
+		transform.Translate(Time.deltaTime*velocity.x, Time.deltaTime*velocity.y, 0f);
+	}
+}
