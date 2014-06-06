fb2f0979db93f2105dc12dcd26c5f724225af4f3
Mihmal <mihmal98@gmail.com>
2014-06-06 15:07:08 +0700

    Update flyobj.py

--- scratches/flyobj.py
+++ scratches/flyobj.py
@@ -1,91 +1,70 @@
-#эта константа отвечает за точность симуляции
-#чем она меньше, тем точнее, но медленнее
-T = 0.1
+using UnityEngine;
+using System.Collections;
 
 
-class FlyObject:
-    mass = 0.0
-    x, y, vx, vy, ax, ay = 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
-    radius = 0
-    surfaceColor = "black"
-    others = []
-    name = ''
+public class FLyObject : MonoBehaviour {
 
-    #конструктор класса
-    def __init__(self, name, mass, x, y, vx, vy):
-        self.mass = mass
-        self.x = x
-        self.y = y
-        self.vx = vx
-        self.vy = vy
-        self.name = name
-        self.others = []
+	public float T = 0.01f;
+	public float mass;
+	public Vector2 velocity;
+	public GameObject[] others;
+	public FLyObject[] othersScripts;
+			
+	private float fx (float localX) {
+		float a = 0;
+		for (int i=0; i<others.Length; i++) {
+			float r = new Vector2(others[i].transform.position.x-localX, others[i].transform.position.y-transform.position.y).magnitude;
+			a += othersScripts[i].mass*(others[i].transform.position.x - transform.position.x)/Mathf.Pow(r, 3);
+		}
+		return a;
+	}
+	
+	float fy (float localY) {
+		float a = 0;
+		for (int i=0; i<others.Length; i++) { 
+			float r = new Vector2(others[i].transform.position.y-localY, others[i].transform.position.x-transform.position.x).magnitude;
+			a += othersScripts[i].mass*(others[i].transform.position.y - transform.position.y)/Mathf.Pow(r, 3);
+		}
+		return a;
+	}
+	
+	void CalcX () {
+		float k1 = T*fx(transform.position.x);
+		float q1 = T*velocity.x;
 
-        print("{0}, ({1}, {2}) v=({3}, {4}), mass={5}"
-        .format(name, x, y, vx, vy, mass))
-
-    #опеделение дистанции до другого объекта
-    def dist(self, other):
-        return math.hypot((self.x - other.x),
-                          (self.y - other.y))
-
-    
-    
-    def calcAccelTo(self, other):
-        self.others.append((other.mass, other.x, other.y))
-
-    def fx(self, local_x):
-        a = 0
-        for (mass, x, y) in self.others:
-            r = math.hypot(x - local_x, y - self.y)
-            a += mass * (x - local_x) / r ** 3
-
-        return a
-
-    def fy(self, local_y):
-        a = 0
-        for (mass, x, y) in self.others:
-            r = math.hypot(x - self.x, y - local_y)
-            a += mass * (y - local_y) / r ** 3
-
-        return a
-
-    def calcX(self):
-        k1 = T * self.fx(self.x)
-        q1 = T * self.vx
-
-        k2 = T * self.fx(self.x + q1 / 2)
-        q2 = T * (self.vx + k1 / 2)
-
-        k3 = T * self.fx(self.x + q2 / 2)
-        q3 = T * (self.vx + k2 / 2)
-
-        k4 = T * self.fx(self.x + q3)
-        q4 = T * (self.vx + k3)
-
-        self.vx += (k1 + 2 * k2 + 2 * k3 + k4) / 6
-        self.x += (q1 + 2 * q2 + 2 * q3 + q4) / 6
-
-    def calcY(self):
-        k1 = T * self.fy(self.y)
-        q1 = T * self.vy
-
-        k2 = T * self.fy(self.y + q1 / 2)
-        q2 = T * (self.vy + k1 / 2)
-
-        k3 = T * self.fy(self.y + q2 / 2)
-        q3 = T * (self.vy + k2 / 2)
-
-        k4 = T * self.fy(self.y + q3)
-        q4 = T * (self.vy + k3)
-
-        self.vy += (k1 + 2 * k2 + 2 * k3 + k4) / 6
-        self.y += (q1 + 2 * q2 + 2 * q3 + q4) / 6
-
-    def update(self):
-        self.calcX()
-        self.calcY()
-        self.others.clear()
-
-    def draw(self, screen)
-        draw.circle(scren, Color("#ffffff"), [self.x, self.y], 2)
+		float k2 = T*fx(transform.position.x + q1/2);
+		float q2 = T*(velocity.x + k1/2);
+		
+		float k3 = T*fx(transform.position.x + q2/2);
+		float q3 = T*(velocity.x + k2/2);
+		
+		float k4 = T*fx(transform.position.x + q3);
+		float q4 = T*(velocity.x + k3);
+		
+		velocity.x += Time.deltaTime*(k1 + 2 * k2 + 2 * k3 + k4) / 6;
+		transform.Translate ((Time.deltaTime * (q1 + 2 * q2 + 2 * q3 + q4) / 6), 0f, 0f);
+	}
+	
+	void CalcY () {
+		float k1 = T*fy(transform.position.y);
+		float q1 = T*velocity.y;
+		
+		float k2 = T*fy(transform.position.y + q1/2);
+		float q2 = T*(velocity.y + k1/2);
+		
+		float k3 = T*fy(transform.position.y + q2/2);
+		float q3 = T*(velocity.y + k2/2);
+		
+		float k4 = T*fy(transform.position.y + q3);
+		float q4 = T*(velocity.y + k3);
+		
+		velocity.y += Time.deltaTime*(k1 + 2 * k2 + 2 * k3 + k4) / 6;
+		transform.Translate (0f, (Time.deltaTime * (q1 + 2 * q2 + 2 * q3 + q4) / 6),  0f);
+	}
+	
+	void FixedUpdate () {
+		CalcX();
+		CalcY();
+		transform.Translate(Time.deltaTime*velocity.x, Time.deltaTime*velocity.y, 0f);
+	}
+}
