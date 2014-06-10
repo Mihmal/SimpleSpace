038973ceebebe9b4a20d55f8e2ec2fe51f1cd7fb
Mihmal <mihmal98@gmail.com>
2014-06-10 16:04:53 +0700

    Добавил базовое управление

--- source/ship.cs
+++ source/ship.cs
@@ -14,13 +14,26 @@ public class Ship : MonoBehaviour{
 	public Vector2 velocity;
 
 	private void CalcThrust () {
-		float additionalVelocity = fuelConsumption * fuelSpeed * Time.deltaTime / (drawMass+fuelMass);
-		velocity.x += additionalVelocity * Mathf.Cos (angle);
-		velocity.y += additionalVelocity * Mathf.Sin (angle);
-		fuelMass -= fuelConsumption;
+		if (fuelMass > 0) {
+			float additionalVelocity = fuelConsumption * fuelSpeed * Time.deltaTime / (drawMass + fuelMass);
+			velocity.x += additionalVelocity * Mathf.Cos (angle);
+			velocity.y += additionalVelocity * Mathf.Sin (angle);
+			fuelMass -= fuelConsumption;
+		}
 	}
-	
+
+	private void proceedInput() {
+		float turningPower = Input.GetAxis("Horizontal")/10f;
+		angle += turningPower;
+		transform.Rotate (0f, 0f, turningPower);
+		float enginePower = Input.GetAxis ("Vertical") / 100f;
+		fuelConsumption += enginePower;
+		if (fuelConsumption < 0) fuelConsumption = 0;
+		else if (fuelConsumption > maxFuelConsumption) fuelConsumption = maxFuelConsumption;
+	}
+
 	void FixedUpdate () {
+		proceedInput ();
 		flyObject.mass = drawMass + fuelMass;
 		CalcThrust ();
 		transform.Translate (Time.deltaTime * velocity.x, Time.deltaTime * velocity.y, 0f);
